9c944efa3b56d9500f26f1c35928e825ad3bc0b4
omar <omar.sharbaji@celebrate.company>
2025-02-12 08:00:30 +0100

    cicd: write cicd for main branch

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello, ArgoCD! pr1!");
+    res.send("Hello, ArgoCD! pr2!");
 });
 
 app.listen(port, () => {
