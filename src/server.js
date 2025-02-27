226abdb1fba7cc917d46e872012210f6a60b96af
omar <omar.sharbaji@celebrate.company>
2025-02-27 09:44:42 +0100

    Change message

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.status(200).send("Hello!");
+    res.status(200).send(`Hello ${process.env.NAME}`);
 });
 
 app.listen(port, () => {
