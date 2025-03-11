974297ba27b6e05380da0cce04d766748ffd1875
omar <omar.sharbaji@celebrate.company>
2025-03-11 12:01:22 +0100

    Add helm chart

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.status(200).send(`Hello TEST`);
+    res.status(500).send(`Hello TEST`);
 });
 
 app.listen(port, () => {
