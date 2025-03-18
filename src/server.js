f0bb60f7720458ad48ba1bde4897f5510ec25763
omar <omar.sharbaji@celebrate.company>
2025-03-18 12:33:04 +0100

    Add readiness probes to the helm chart

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ receiveMessages();
 
 app.get("/", (req, res) => {
     console.log('a new request has been Logged')
-    res.status(200).send(`Hello Argo 55555`);
+    res.status(200).send(`Hello Argo 333`);
 });
 
 app.listen(port, () => {
